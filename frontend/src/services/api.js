3cae923615ca4b3c31ce9fb80971a04ccd324d67
Suhani Maurya <suhanimaurya72@gmail.com>
2026-06-06 16:21:13 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "https://github-profile-finder-zlwv.onrender.com",
+  baseURL: "https://github-profile-finder-zlwv.onrender.com/api/auth",
 });
 
 
